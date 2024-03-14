db3addb87f7cd5cf8e3f4584274df9fd9bfb669d
Raidzin <60663550+Raidzin@users.noreply.github.com>
2024-03-14 16:18:00 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="moderator">
+            <div><strong>Имя пользователя: </strong>goncharukak</div>
+            <div><strong>Роль: </strong>admin</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        addUser() {
+            this.usersCout++;
+        }
+    }
+}
+</script>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+</style>
