bf356f1956d2f542285cbf1ebe219a447092121c
Raidzin <60663550+Raidzin@users.noreply.github.com>
2024-03-14 16:55:35 +0300

    add cdn tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
-    <div>
-        <div class="moderator">
-            <div><strong>Имя пользователя: </strong>goncharukak</div>
-            <div><strong>Роль: </strong>admin</div>
+    <div class="flex items-center justify-center">
+        <div class="w-full max-w-screen-md">
+            <form class="w-full m-8">
+                <input type="text" placeholder="Имя пользователя" class="w-full mb-2">
+                <input type="select" placeholder="роль" class="w-full mb-2">
+            </form>
+            <div class="my-8 mx-4 w-full border-4 rounded-lg border-blue-900 hover:border-lime-200  duration-300" v-for="user in users">
+                <div><strong>Имя пользователя: </strong>{{ user.username }}</div>
+                <div><strong>Роль: </strong>{{ user.role }}</div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            users: [
+                {username: "goncharukak", role: "admin"},
+                {username: "migunovala", role: "moderator"},
+            ]
+        }
+    },
     methods: {
         addUser() {
             this.usersCout++;
